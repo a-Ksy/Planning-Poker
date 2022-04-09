acce44f0afc4c2e8f5ee69cdbc8af3e63f5c6521
Atahan Aksoy <aaksoy17@ku.edu.tr>
2022-04-09 14:48:23 +0300

    Change CreateRoom API url

--- webapp/src/features/createRoom/actions.tsx
+++ webapp/src/features/createRoom/actions.tsx
@@ -8,7 +8,7 @@ export const setUsername = createAction<string>("createRoom/setUsername");
 export const createRoom = createAsyncThunk(
   "createRoom/createRoom",
   async ({ roomName, username }: { roomName: string; username: string }) => {
-    const response = await axios.post("http://localhost:8080/api/room", {
+    const response = await axios.post("http://localhost:8080/api/room/", {
       roomName,
       username,
     });
