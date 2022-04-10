2bfcd8e1541026ec5e1fd8075df2f7279482cee2
Atahan Aksoy <aaksoy17@ku.edu.tr>
2022-04-11 00:30:04 +0300

    Change PokerTable color to chakraui color

--- webapp/src/components/PokerTable.tsx
+++ webapp/src/components/PokerTable.tsx
@@ -3,12 +3,11 @@ import { Box, Center, Text, useColorMode } from "@chakra-ui/react";
 export const PokerTable = () => {
   return (
     <Box
-      bg="#D7E9FF"
+      bg="blue.300"
       w={{ base: "50vw", md: "30vw", lg: "20vw" }}
       h="18vh"
       p={4}
       borderRadius="3xl"
-      borderWidth="1px"
       alignItems="center"
     >
       <Center h="100%">
